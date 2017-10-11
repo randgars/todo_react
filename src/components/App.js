5a2b26456b8a919e89f2e92fb0823c1732b1c864
Pavel Anosov <pavel.anosov@effective-soft.com>
2017-10-11 11:21:05 +0300

    create project

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.scss';
+
+class AppComponent extends React.Component {
+
+  render() {
+    return (
+      <div className="index">
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
